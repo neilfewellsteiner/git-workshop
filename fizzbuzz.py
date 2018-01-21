979cfceafb7032f8e9d5a562c8d1ef2d190c2346
Neil Steiner <nfs15@case.edu>
2018-01-21 14:42:23 -0500

    Added fizzbuzzbizz buzzbizz and fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,13 @@
 
 def fizzbuzz(n):
     for i in range(1, n + 1):
-        if i % 15 == 0:
+		if i % 105 == 0:
+			print "fizzbuzzbizz"
+		elif i % 35 == 0:
+			print "buzzbizz"
+		elif i % 21 == 0:
+			print "fizzbuzz"
+        elif i % 15 == 0:
             print "fizzbuzz"
         elif i % 3 == 0:
             print "fizz"
